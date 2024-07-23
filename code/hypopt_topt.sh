c53b83844318a5c15eb36e71964361c20afc5032
Sizhe Qiu <bras5181@htc-login04.arcus-htc.arc.local>
2024-07-23 12:49:41 +0100

    hyopt_topt_2round_23072024

--- code/hypopt_topt.sh
+++ code/hypopt_topt.sh
@@ -8,21 +8,22 @@
 
 module load PyTorch/1.7.1-fosscuda-2020b
 
-samples=("1" "2" "3" "4" "5" "6" "7" "8" "9")
-for index in ${samples[@]}
-do
-echo $index
-start_time=$(date +%s)
+#samples=("1" "2" "3" "4" "5" "6" "7" "8" "9")
+#for index in ${samples[@]}
+#do
+#echo $index
+#start_time=$(date +%s)
 
-eval "python run_train.py --task topt --train_path ../data/Topt/train_os.csv --test_path ../data/Topt/test.csv \
-   --param_dict_pkl ../data/hyparams/params_${index}.pkl"
+#eval "python run_train.py --task topt --train_path ../data/Topt/train_os.csv --test_path ../data/Topt/test.csv \
+#   --param_dict_pkl ../data/hyparams/params_${index}.pkl"
     
-end_time=$(date +%s)
-elapsed=$(( end_time - start_time ))
-echo $elapsed
-done
+#end_time=$(date +%s)
+#elapsed=$(( end_time - start_time ))
+#echo $elapsed
+#done
 
 
+python run_train.py --task topt --train_path ../data/Topt/train_os.csv --test_path ../data/Topt/test.csv --param_dict_pkl ../data/hyparams/params_5.pkl
 
 
 
